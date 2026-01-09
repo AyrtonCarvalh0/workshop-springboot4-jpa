7d53ef356ca8d9be4fe3a8b69b79d99859c88105
Ayrton Carvalho <cayrton021@gmail.com>
2026-01-09 18:25:13 -0300

    OrderStatus enum

--- src/main/java/com/yrtweb/course/config/testConfig.java
+++ src/main/java/com/yrtweb/course/config/testConfig.java
@@ -1,5 +1,6 @@
 package com.yrtweb.course.config;
 
+import com.yrtweb.course.entities.Enum.OrderStatus;
 import com.yrtweb.course.entities.Order;
 import com.yrtweb.course.entities.User;
 import com.yrtweb.course.repositories.OrderRepository;
@@ -30,9 +31,9 @@ public class testConfig implements CommandLineRunner {
         User u2  = new User(null , "Alex Brown "  , "alex@gmail.com "  , "9211231" , "133211");
         ur.saveAll(Arrays.asList(u1,u2));
 
-    Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-    Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
-    Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+    Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID ,u1);
+    Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT ,u2);
+    Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT ,u1);
     or.saveAll(Arrays.asList(o1,o2,o3));
 
 
--- src/main/java/com/yrtweb/course/entities/Order.java
+++ src/main/java/com/yrtweb/course/entities/Order.java
@@ -1,14 +1,12 @@
 package com.yrtweb.course.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.yrtweb.course.entities.Enum.OrderStatus;
 import jakarta.persistence.*;
 
-import java.io.Serial;
 import java.io.Serializable;
 import java.time.Instant;
 import java.util.Objects;
-import java.util.TimeZone;
 
 @Entity
 @Table(name = "tb-order")
@@ -22,6 +20,8 @@ public class Order implements Serializable {
     @JsonFormat(shape = JsonFormat.Shape.STRING,pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'",  timezone = "GMT")
     private Instant moment;
 
+    private Integer orderStatus;
+
 
     @ManyToOne
     @JoinColumn(name = "client_id")
@@ -30,9 +30,10 @@ public class Order implements Serializable {
     public Order(){
 
     }
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
 
@@ -48,6 +49,10 @@ public class Order implements Serializable {
         return client;
     }
 
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOff(orderStatus);
+    }
+
     public void setId(Long id) {
         this.id = id;
     }
@@ -58,6 +63,12 @@ public class Order implements Serializable {
 
     public void setClient(User client) {
         this.client = client;
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if(orderStatus !=  null) {
+            this.orderStatus = orderStatus.getCode();
+        }
     }
 
     @Override
