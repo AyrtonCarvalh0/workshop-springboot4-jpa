57af3f4538aa284dc437305a794c24660f7437de
Ayrton Carvalho <cayrton021@gmail.com>
2026-01-18 15:19:41 -0300

    User insert

--- src/main/java/com/yrtweb/course/resources/UserResource.java
+++ src/main/java/com/yrtweb/course/resources/UserResource.java
@@ -6,11 +6,11 @@ import com.yrtweb.course.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.ServletRequestBindingException;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -32,5 +32,11 @@ public class UserResource {
     public ResponseEntity<User>findById(@PathVariable Long id){
         User user = us.findById(id);
         return ResponseEntity.ok().body(user);
+    }
+    @PostMapping
+    public ResponseEntity<User> insert(@RequestBody User obj){
+        obj = us.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).body(obj);
     }
 }
